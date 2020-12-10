cd6b5eee599cbb059b1f36411fe8e13bca974a03
Rebecca Connell <ed18r22c@leeds.ac.uk>
2020-12-10 09:08:47 +0000

    tidying up code

--- add_file.cpp
+++ add_file.cpp
@@ -3,18 +3,13 @@
 #include <QMessageBox>
 
 void AddFile::showMessage() {
-    /*QMessageBox *popUp= new QMessageBox;
-    popUp->addButton(QMessageBox::Ok);
-    popUp->setWindowTitle("File Chooser");
-    popUp->setText("This would be a file chooser");
-    popUp->show();*/
     if(QMessageBox::information(this, "Tomeo", "This is a file chooser")==QMessageBox::Ok){
         addTheFile();
     }
-    //connect(&popUp, SIGNAL(buttonClicked()), this, SLOT(addTheFile()));
 }
 
 void AddFile:: addTheFile(){
+    //initialising button
     TheButton *button = new TheButton(videoButtonsWidget);
     button->connect(button, SIGNAL(jumpTo(TheButtonInfo* )), player, SLOT (jumpTo(TheButtonInfo* ))); // when clicked, tell the player to play.
     buttons.push_back(button);
@@ -22,18 +17,14 @@ void AddFile:: addTheFile(){
     QLabel* buttonTitle = new QLabel(QString(videos[0].url->fileName()));
     buttonTitle->setAlignment(Qt::AlignCenter);
 
+    //making compliant with new organisation feature
     QVBoxLayout* nextPair = new QVBoxLayout();
     nextPair->addWidget(button);
     nextPair->addWidget(buttonTitle);
     pairs->push_back(nextPair);
     videoButtonsLayout->addLayout(nextPair);
     button->init(&videos.at(0));
-    //videoButtonsLayout->addWidget(button);
-    //button->init(&videos.at(0));
 
-    QMessageBox *popUp= new QMessageBox;
-    popUp->setText("Test!");
-    popUp->show();
 }
 
 
